108f02323b39739f1e170f036970559dc38039d4
AbdAllAH-ElRhmany <63827604+AbdAllAH-ElRhmany@users.noreply.github.com>
2023-04-15 06:33:30 +0200

    Update

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -13,7 +13,7 @@ dotenv.config({ path: './config.env' });
 // );
 
 mongoose
-  .connect(process.env.DATABASE_LOCAL, {
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-process.on('uncaughtException', (err) => {
-  console.log(err.name, err.message);
-  console.log('UNHANDLER EXCEPTION!! Shutting down...');
-  process.exit(1);
-});
+// process.on('uncaughtException', (err) => {
+//   console.log(err.name, err.message);
+//   console.log('UNHANDLER EXCEPTION!! Shutting down...');
+//   process.exit(1);
+// });
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 const port = process.env.PORT || 3000;
 
 mongoose
-  .connect(process.env.DATABASE_LOCAL, {
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: true,
@@ -20,17 +20,17 @@ mongoose
   .then((con) => {
     // console.log(con);
     console.log('DB Connected Successfly');
-  });
-// .catch((err) => console.log("Connection Error"));
+  })
+  .catch((err) => console.log('Connection Error'));
 
 const server = app.listen(port, (req, res) => {
-  // console.log('App running on port ' + port);
+  console.log('App running on port ' + port);
 });
 
-process.on('unhandledRejection', (err) => {
-  // console.log(err.name, err.message);
-  // console.log('UNHANDLER REHECTION!! Shutting down...');
-  server.close(() => {
-    process.exit(1);
-  });
-});
+// process.on('unhandledRejection', (err) => {
+//   // console.log(err.name, err.message);
+//   // console.log('UNHANDLER REHECTION!! Shutting down...');
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
